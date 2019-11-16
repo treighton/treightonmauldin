4a449251e1dc96bc2384d0bda3958855aefa4d80
treighton <treighton@gmail.com>
2019-11-16 09:53:31 -0800

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,11 @@ module.exports = {
         background_color: `##111F51`,
         theme_color: `##111F51`,
         display: `minimal-ui`,
-        icon: `src/images/profile.png`, // This path is relative to the root of the site.
+        icon: `src/images/tr8.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
