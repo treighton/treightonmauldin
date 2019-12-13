287f263b1051fa568bc23a43ae45736c7e3f5583
Treighton <treighton@pagedesigngroup.com>
2019-12-13 10:27:37 -0800

    i fucked up

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,55 +6,55 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="container">
-      <div className="row my-4">
-        <div className="col-md-6">
-          <p className="accent-text">SACRAMENTO</p>
-          <h1 className="display-1">
-            DEV
+	<Layout>
+		<SEO title="Home" />
+		<div className="container">
+			<div className="row my-4">
+				<div className="col-md-6">
+					<p className="accent-text">SACRAMENTO</p>
+					<h1 className="display-1">
+						DEV
             <br />
-            ELOP
+						ELOP
             <br />
-            ER
+						ER
           </h1>
-        </div>
-        <div className="col-md-6">
-          <p className="accent-frame">
-            Hi! I'm Treighton Mauldin. I am a{" "}
-            <span className="highlight">developer, teacher, writer</span>,
-            aspiring personal trainer, and amateur photographer in Sacrmento,
+				</div>
+				<div className="col-md-6">
+					<p className="accent-frame">
+						Hi! I'm Treighton Mauldin. I am a{" "}
+						<span className="highlight">developer, teacher, writer</span>,
+						aspiring personal trainer, and amateur photographer in Sacrasmento,
             Ca. As a Web Developer, with about{" "}
-            <span className="highlight">five years of experience</span> with
+						<span className="highlight">five years of experience</span> with
             PHP, MySQL, JavaScript, HTML, CSS, and WordPress, and{" "}
-            <span className="highlight">
-              over two years experience working with, and teaching
+						<span className="highlight">
+							over two years experience working with, and teaching
             </span>{" "}
-            React, Node, and MongoDB.
+						React, Node, and MongoDB.
           </p>
-          <div className="read-on">
-            <h3 className="display-2 text-right">
-              <em>REACT</em>
-            </h3>
-            <h3 className="display-2">
-              <em>NODE</em>
-            </h3>
-            <h3 className="display-2 text-right">
-              <em>PHP</em>
-            </h3>
-          </div>
-        </div>
-      </div>
-      <div className="row mb-5">
-        <div className="col">
-          <p className="accent-text-alt">
-            <span className="accent-frame">LET'S</span> GET D1G1TAL
+					<div className="read-on">
+						<h3 className="display-2 text-right">
+							<em>REACT</em>
+						</h3>
+						<h3 className="display-2">
+							<em>NODE</em>
+						</h3>
+						<h3 className="display-2 text-right">
+							<em>PHP</em>
+						</h3>
+					</div>
+				</div>
+			</div>
+			<div className="row mb-5">
+				<div className="col">
+					<p className="accent-text-alt">
+						<span className="accent-frame">LET'S</span> GET D1G1TAL
           </p>
-        </div>
-      </div>
-    </div>
-  </Layout>
+				</div>
+			</div>
+		</div>
+	</Layout>
 )
 
 export default IndexPage
